602d7ccd6a1da55092e8d5b853f89274232dcb50
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-03-25 23:56:02 +0600

    implement findAll method in employee repository

--- src/main/java/com/shahaz/cruddemo/dao/EmployeeDaoJpaImpl.java
+++ src/main/java/com/shahaz/cruddemo/dao/EmployeeDaoJpaImpl.java
@@ -1,13 +1,31 @@
 package com.shahaz.cruddemo.dao;
 
 import com.shahaz.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public class EmployeeDaoJpaImpl implements EmployeeDao {
+
+    EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDaoJpaImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
     @Override
     public List<Employee> findAll() {
-        return null;
+        //create query
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM employee", Employee.class);
+
+        //execute query and get result list
+        List<Employee> employeeList =   theQuery.getResultList();
+
+        //return list
+        return  employeeList;
     }
 }
